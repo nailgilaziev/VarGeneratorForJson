ebded960886dbd2bf47c1a16b87b11b4236aa1ac
Nail Gilaziev <nail.gilaziev@gmail.com>
2013-01-01 21:49:59 +0400

    generate Table class code
    Изменен интерфейс на вкладки. генерирование кода для класса наследуемо
    от Table

--- VarGeneratorForJson/MainWindow.xaml.cs
+++ VarGeneratorForJson/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace VarGeneratorForJson
         public MainWindow()
         {
             InitializeComponent();
-            temp.Text = @"id
+            tb_exclusionVars.Text = @"id
 Id
 confirmed
 num
@@ -34,29 +34,100 @@ num
         {
             try
             {
-                String varRes = "";
-                String parseRes = "";
-                String[] row = orig.Text.Replace("\"","").Split('\n');
-                foreach (var item in row)
+                String tableName=tb_jtableName.Text;
+                List<string> vars=getJSONObjectVars(tb_JSONObjectStruct.Text.Replace("\"", ""));
+                List<string> javaVars = getJavaVars(vars);
+
+                List<string> javaVarsDeclaration = getJavaVarsDeclaration(javaVars,vars);
+                List<string> javaJsonParse = getJsonParseCode(javaVars, vars);
+                List<string> tvars = getTableVarsDeclaration(vars);                
+                
+                tb_jvars.Text = listToString(javaVarsDeclaration);
+                tb_jparse.Text = listToString(javaJsonParse);
+                tb_tvars.Text = listToString(tvars);
+
+                if (tableName != "")
                 {
-                    String variable = item.Substring(0, item.IndexOf(":")).Trim();
-                    String javaVarName = createJavaVar(variable);
-                    varRes += String.Format("protected {0} {1};\n", getPossiblyVarType(variable, false), javaVarName);
-                    parseRes += String.Format("{0} = j.get{1}(\"{2}\");\n", javaVarName, getPossiblyVarType(variable, true), variable);
+                    tb_tcolumns.Text = getTableColumnsCreateCode(tableName, vars);
+                    tb_tmodel_create.Text = getCreateModelCode(tableName, vars);
+                    tb_tmodel_insert.Text = getInsertModelCode(tableName, vars, javaVars);
+
+                    tb_table_code.Text = getTableCode(tableName, vars, javaVars);
                 }
-                result.Text = varRes + "\n" + parseRes;
+                if (tabControl.SelectedIndex == 0)
+                    tabControl.SelectedIndex = 1;
+
+
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
             }
-            
+
+        }
+        /**
+         * Возвращает список переменных JSONObject'a котторый был передан для parsing'a
+         */
+        private List<string> getJSONObjectVars(String varDescriptionRows)
+        {
+            List<string> vars = new List<string>();
+            String[] rows = varDescriptionRows.Replace("\"", "").Split('\n');
+            foreach (var row in rows)
+            {
+                String variable = row.Substring(0, row.IndexOf(":")).Trim();
+                vars.Add(variable);
+            }
+            return vars;
         }
 
-        private string getPossiblyVarType(string variable,bool upperFirstLetter)
+        /**
+         * Возвращает список имен Java переменных 
+         */
+        private List<string> getJavaVars(List<string> vars)
+        {
+            List<string> javaVars = new List<string>();
+            foreach (var variable in vars)
+            {
+                String javaVar = createJavaVar(variable);
+                javaVars.Add(javaVar);
+            }
+            return javaVars;
+        }
+
+        /**
+         * Возвращает описание Java переменных 
+         */
+        private List<string> getJavaVarsDeclaration(List<string> javaVars, List<string> vars)
+        {
+            List<string> javaVarsDeclaration = new List<string>();
+            for (int i = 0; i < javaVars.Count; i++)
+            {
+                String t = String.Format("protected {0} {1};", getPossiblyVarType(vars[i], false), javaVars[i]);
+                javaVarsDeclaration.Add(t);
+            }
+            return javaVarsDeclaration;
+        }
+
+        /**
+         * Возвращает код парсинга JSONObject'a в java переменные
+         */
+        private List<string> getJsonParseCode(List<string> javaVars, List<string> vars)
+        {
+            List<string> parseList = new List<string>();
+            for (int i = 0; i < javaVars.Count; i++)
+            {
+                String t = String.Format("{0} = j.get{1}(\"{2}\");", javaVars[i], getPossiblyVarType(vars[i], true), vars[i]);
+                parseList.Add(t);
+            }
+            return parseList;
+        }
+
+
+        private string getPossiblyVarType(string variable, bool upperFirstLetter)
         {
             String[] sep = new String[] { "\r\n" };
-            String[] temps = temp.Text.Split(sep,StringSplitOptions.RemoveEmptyEntries);
+            String[] temps = tb_exclusionVars.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries);
             foreach (var item in temps)
             {
                 if (variable.Contains(item))
@@ -67,22 +138,144 @@ num
 
         private string createJavaVar(string variable)
         {
-            try
+            for (int i = variable.IndexOf('_'); i < variable.Length && i != -1; i = variable.IndexOf('_'))
             {
-                for (int i = variable.IndexOf('_'); i < variable.Length && i != -1; i = variable.IndexOf('_'))
-                {
-                    String s1 = variable.Substring(0, i);
-                    String letter = (variable[i + 1].ToString()).ToUpper();
-                    String s2 = variable.Substring(i + 2);
-                    variable = s1 + letter + s2;
-                }
-                return variable;
+                String s1 = variable.Substring(0, i);
+                String letter = (variable[i + 1].ToString()).ToUpper();
+                String s2 = variable.Substring(i + 2);
+                variable = s1 + letter + s2;
             }
-            catch (Exception ex)
+            return variable;
+        }
+
+        /// <summary>
+        /// описания переменных  в классе Table
+        /// </summary>
+        /// <param name="vars"></param> имена переменных json объекта
+        /// <returns></returns>
+        private List<string> getTableVarsDeclaration(List<string> vars)
+        {
+            List<string> tVars = new List<string>();
+            foreach (var variable in vars)
             {
-                return ex.ToString();
+                var varDeclaration = String.Format("public static final String {0}=\"{1}\";",variable.ToUpper(),variable);
+                tVars.Add(varDeclaration);
             }
-            
+            return tVars;
+        }
+
+        /// <summary>
+        ////Код создания колонок
+        /// </summary>
+        /// <param name="vars">имена переменных json объекта</param>
+        /// <returns></returns>
+        private string getTableColumnsCreateCode(String tableName, List<string> vars)
+        {
+            String code = String.Format(@"
+public T{0}(Database database) {{
+super(database);
+table_name=""{0}"";
+addColumns(
+", tableName);
+            for (int i = 0; i < vars.Count; i++)
+            {
+                String lineTrail = i < vars.Count - 1 ? "," : "";
+                if (getPossiblyVarType(vars[i], true) == "String")
+                    code += String.Format("new Column({0}){1}\n", vars[i].ToUpper(), lineTrail);
+                else
+                    code += String.Format("new Column({0},Types.INTEGER){1}\n", vars[i].ToUpper(), lineTrail);
+            }
+            code += @");
+}";
+            return code;
+        }
+
+        private string getCreateModelCode(String tableName, List<string> vars)
+        {
+            String code = String.Format(@"
+
+private J{0} get{0}(Cursor c){{
+return new J{0}(
+", tableName.Substring(0,tableName.Length-1));
+            for (int i = 0; i < vars.Count; i++)
+            {
+                String lineTrail = i < vars.Count - 1 ? "," : "";
+                if (getPossiblyVarType(vars[i], true) == "String")
+                    code += String.Format("c.getString(c.getColumnIndex({0})){1}\n", vars[i].ToUpper(), lineTrail);
+                else
+                    code += String.Format("c.getInt(c.getColumnIndex({0})){1}\n", vars[i].ToUpper(), lineTrail);
+            }
+            code += @");
+}";
+            return code;
+        }
+
+        private string getInsertModelCode(String tableName, List<string> vars, List<string> javaVars)
+        {
+            String tableNameLower = tableName.ToLower();
+            tableNameLower=tableNameLower.Substring(0, tableNameLower.Length - 1);
+            String code = String.Format(@"
+
+public Long insert(J{0} {1}){{
+ContentValues cv=new ContentValues();
+", tableName.Substring(0, tableName.Length - 1), tableNameLower);
+            for (int i = 0; i < vars.Count; i++)
+            {
+                String javaVarUpFirstLetter = javaVars[i];
+                javaVarUpFirstLetter = javaVarUpFirstLetter[0].ToString().ToUpper() + javaVarUpFirstLetter.Substring(1);
+                code += String.Format("cv.put({0},{1}.get{2}());\n", vars[i].ToUpper(), tableNameLower, javaVarUpFirstLetter);
+            }
+            code += @"
+Long id=insert(cv);						
+return id;
+}";
+            return code;
+        }
+
+        private string getOtherCode(String tableName)
+        {
+            return String.Format(@"
+
+    public List<J{0}> get(String  whereClause) {{
+		List<J{0}> t=new ArrayList<J{0}>();
+		Cursor cursor=select(whereClause);						
+		while(cursor.moveToNext())
+			t.add(get{0}(cursor));			
+		cursor.close();			
+		db.close();
+		return t;
+	}}
+
+	public List<J{0}> getAll() {{
+		return get(null);
+	}}
+
+	public void deleteAll() {{
+		delete(null);
+	}}
+", tableName.Substring(0, tableName.Length - 1));
+        }
+
+        string getTableCode(String tableName, List<string> vars, List<string> javaVars)
+        {
+            String code = String.Format(@"public class T{0} extends Table {{
+
+", tableName);
+            code += listToString(getTableVarsDeclaration(vars))
+            + getTableColumnsCreateCode(tableName, vars)
+            + getCreateModelCode(tableName, vars)
+            + getInsertModelCode(tableName, vars, javaVars)
+            + getOtherCode(tableName)
+            + "}";
+            return code;
+        }
+
+        string listToString(List<string> list)
+        {
+            string s = "";
+            foreach (var item in list)
+                s += item + "\n";
+            return s;
         }
     }
 }
